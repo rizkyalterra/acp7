3748cec56326865d64a46cd47895bfc0b4b265ff
Rizky <rizky.kurniawan@alterra.id>
2021-04-15 21:03:43 +0700

    merge stash file

--- main.go
+++ main.go
@@ -4,5 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+<<<<<<< Updated upstream
 	fmt.Println("Hello B")
+=======
+	fmt.Println("H")
+	fmt.Println("G")
+>>>>>>> Stashed changes
 }
